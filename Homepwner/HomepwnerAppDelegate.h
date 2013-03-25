744b281385ac58a6eb69ff38beb217e3c3af9b8e
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-03-25 17:10:04 -0500

    init
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by kgaddy on 3/25/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
